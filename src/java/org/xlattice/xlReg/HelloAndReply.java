d0f2ace7744cf540208739079013d8ee995bd795
Jim Dixon <jddixon@gmail.com>
2014-02-09 17:25:27 -0800

    fleshed out ClientEncodeHello()

--- src/java/org/xlattice/xlReg/HelloAndReply.java
+++ src/java/org/xlattice/xlReg/HelloAndReply.java
@@ -4,10 +4,20 @@ package org.xlattice.xlReg;
 import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
 
 import javax.crypto.Cipher;
+
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.crypto.AsymmetricBlockCipher;
+import org.bouncycastle.crypto.digests.SHA1Digest;
+import org.bouncycastle.crypto.encodings.OAEPEncoding;
+import org.bouncycastle.crypto.engines.RSAEngine;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.util.PublicKeyFactory;
 
 /**
  *
@@ -16,7 +26,7 @@ import javax.crypto.Cipher;
 
 public class HelloAndReply {
 
-    private final int value;
+    private int value;      // XXX never set
 
     // CONSTRUCTORS /////////////////////////////////////////////////
 
@@ -38,33 +48,34 @@ public class HelloAndReply {
     // OTHER METHODS ////////////////////////////////////////////////
     
    
-    public byte[] ClientEncodeHello(version DecimalVersion, 
-            ck *rsaPublicKey) throws Exception {
+    public byte[] ClientEncodeHello(DecimalVersion version, 
+            PublicKey ck) throws Exception {
 
         // data is 56 random bytes + 4 byte version where 
         // 56 = iv + key + salt = 16 + 32 + 8
-        //
-        //
+        byte[] data = new byte[56];
+        new SecureRandom().nextBytes(data);
+        
         //BOUNCY CASTLE:
-        SubjectPublicKeyInfo publicKeyInfo = new SubjectPublicKey(
+        SubjectPublicKeyInfo publicKeyInfo = new SubjectPublicKeyInfo(
                 ASN1Sequence.getInstance(ck.getEncoded()));
         AsymmetricKeyParameter param = PublicKeyFactory.
             createKey(publicKeyInfo);
         AsymmetricBlockCipher cipher = new OAEPEncoding(new RSAEngine(),
-                new SHA1Digest();
-        return cipher.processBlock(data, 0, 32)
+                new SHA1Digest());
+        return cipher.processBlock(data, 0, 32);
     }
     
     
     // public boolean equals(Object o) {
-    
+   
+    // }
+
     // SERIALIZATION ////////////////////////////////////////////////
 
-    // Convert the HelloAndReply to dotted string format.  We always include
-    // at least the two leading parts, so version 1.0.0.0 in string form 
-    // is "1.0".
     
     // public String toString() {
+    // }
 }
 
 
