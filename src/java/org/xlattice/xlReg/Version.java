b1393a8fae119c1f8ab3dea09747e4b0fea106da
Jim Dixon <jddixon@gmail.com>
2015-06-19 10:51:48 -0700

    add Version.java, edit build.xml properties

---
+++ src/java/org/xlattice/xlReg/Version.java
@@ -0,0 +1,16 @@
+/* Version.java */
+package org.xlattice.xlReg;
+
+/**
+ * THIS CLASS IS AUTOMATICALLY GENERATED.  DO NOT EDIT
+ */
+public class Version extends org.xlattice.Version {
+
+    public Version () {
+        super("org.xlattice.xlReg", // package
+            0,                       // minor
+            1,                       // minor
+            2,                       // decimal
+            0);                      // build
+    }
+};
